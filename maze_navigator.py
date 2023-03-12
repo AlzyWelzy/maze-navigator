04479d78b22a9425a4bb009b00db276104b81151
AlzyWelzy <welzyalzy@gmail.com>
2023-03-12 20:34:00 +0530

    adding code for maze

--- maze_navigator.py
+++ maze_navigator.py
@@ -16,12 +16,22 @@ maze = [
 ]
 
 
+def print_maze(maze, stdscr, path=[]):
+    BLUE = curses.color_pair(1)
+    RED = curses.color_pair(2)
+
+    for i, row in enumerate(maze):
+        for j, value in enumerate(row):
+            stdscr.addstr(i, j * 3, value, BLUE)
+
+
 def main(stdscr):
     curses.init_pair(1, curses.COLOR_BLUE, curses.COLOR_BLACK)
     blue_and_black = curses.color_pair(1)
 
     stdscr.clear()
-    stdscr.addstr(0, 0, "hello world!", blue_and_black)
+    # stdscr.addstr(0, 0, "hello world!", blue_and_black)
+    print_maze(maze, stdscr)
     stdscr.refresh()
     stdscr.getch()
 
